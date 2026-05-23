4a95e691f8a1b1fec42b5fb87829d9478c7715a4
Kalid Mishal <143897849+KalidMishal@users.noreply.github.com>
2026-05-23 13:37:08 +0530

    Add files via upload

---
+++ dist/server/assets/_tanstack-start-manifest_v-CFEUIx7_.js
@@ -0,0 +1,4 @@
+const tsrStartManifest = () => ({ routes: { __root__: { filePath: "C:/Users/User/Desktop/texas-beauty/texas-beauty-oasis-main/src/routes/__root.tsx", children: ["/", "/sitemap.xml"], assets: void 0, preloads: ["/assets/index-BFJR4Zc1.js"] }, "/": { filePath: "C:/Users/User/Desktop/texas-beauty/texas-beauty-oasis-main/src/routes/index.tsx", children: void 0, assets: void 0, preloads: ["/assets/index-Br8uphIp.js"] } }, clientEntry: "/assets/index-BFJR4Zc1.js" });
+export {
+  tsrStartManifest
+};
